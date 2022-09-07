9b0b2cbbe46a709b839572b77272729649a9e68a
XOctOpus1 <dd111320@gmail.com>
2022-09-07 10:10:08 +0300

    debug lab1

--- Ruby/Lab_1/main.rb
+++ Ruby/Lab_1/main.rb
@@ -7,7 +7,7 @@ $sh = gets.chomp
 j = $sh.to_s.to_f
 
 
-V = Math.cos(24*Math::PI/2)+((Math.tan(Math.log(x**3,Math.exp(1))**5) + 4.2*(10**(-2.8)))/(Math.sqrt((x + Math.exp(j)).abs)))
+V = Math.cos(24*Math::PI/2) + ((Math.tan(Math.log(x**3,Math.exp(1)))**5 + 4.2*(10**(-2.8)))/(Math.sqrt((x + Math.exp(j)).abs)))
 
 
 puts V
