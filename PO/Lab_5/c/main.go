9b002e748dae8bfef335ee8ed476c32e3cfe99a4
XOctOpus1 <dd111320@gmail.com>
2022-10-06 19:10:30 +0300

    update go in lab 5 PO

--- PO/Lab_5/c/main.go
+++ PO/Lab_5/c/main.go
@@ -8,11 +8,10 @@ import (
 )
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
 	var wg sync.WaitGroup
 	wg.Add(3)
-	var a [10]int
-	var b [10]int
-	var c [10]int
+	var a, b, c [10]int
 	for i := 0; i < 10; i++ {
 		a[i] = rand.Intn(10)
 		b[i] = rand.Intn(10)
@@ -21,64 +20,46 @@ func main() {
 	go func() {
 		defer wg.Done()
 		for {
-			sum1 := 0
-			sum2 := 0
-			sum3 := 0
+			sum := 0
 			for i := 0; i < 10; i++ {
-				sum1 += a[i]
-				sum2 += b[i]
-				sum3 += c[i]
+				sum += a[i]
 			}
-			if sum1 == sum2 && sum2 == sum3 {
-				fmt.Println("Sum of a:", sum1)
-				fmt.Println("Sum of b:", sum2)
-				fmt.Println("Sum of c:", sum3)
+			if sum == 50 {
+				fmt.Println("a = ", a)
 				break
+			} else {
+				a[rand.Intn(10)] += rand.Intn(2)*2 - 1
 			}
-			a[rand.Intn(10)] += rand.Intn(2)*2 - 1
-			time.Sleep(100 * time.Millisecond)
 		}
 	}()
 	go func() {
 		defer wg.Done()
 		for {
-			sum1 := 0
-			sum2 := 0
-			sum3 := 0
+			sum := 0
 			for i := 0; i < 10; i++ {
-				sum1 += a[i]
-				sum2 += b[i]
-				sum3 += c[i]
+				sum += b[i]
 			}
-			if sum1 == sum2 && sum2 == sum3 {
-				fmt.Println("Sum of a:", sum1)
-				fmt.Println("Sum of b:", sum2)
-				fmt.Println("Sum of c:", sum3)
+			if sum == 50 {
+				fmt.Println("b = ", b)
 				break
+			} else {
+				b[rand.Intn(10)] += rand.Intn(2)*2 - 1
 			}
-			b[rand.Intn(10)] += rand.Intn(2)*2 - 1
-			time.Sleep(100 * time.Millisecond)
 		}
 	}()
 	go func() {
 		defer wg.Done()
 		for {
-			sum1 := 0
-			sum2 := 0
-			sum3 := 0
+			sum := 0
 			for i := 0; i < 10; i++ {
-				sum1 += a[i]
-				sum2 += b[i]
-				sum3 += c[i]
+				sum += c[i]
 			}
-			if sum1 == sum2 && sum2 == sum3 {
-				fmt.Println("Sum of a:", sum1)
-				fmt.Println("Sum of b:", sum2)
-				fmt.Println("Sum of c:", sum3)
+			if sum == 50 {
+				fmt.Println("c = ", c)
 				break
+			} else {
+				c[rand.Intn(10)] += rand.Intn(2)*2 - 1
 			}
-			c[rand.Intn(10)] += rand.Intn(2)*2 - 1
-			time.Sleep(100 * time.Millisecond)
 		}
 	}()
 	wg.Wait()
